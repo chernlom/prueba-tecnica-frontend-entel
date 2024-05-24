58f0153f5f41d9704461ccc23f33efea7eb632c9
chernlom <christian.hernandez@latitudde.com>
2024-05-24 09:22:01 -0500

    project clean up

--- src/components/UserList.jsx
+++ src/components/UserList.jsx
@@ -3,31 +3,7 @@ import React from 'react';
 const API_URL = 'https://jsonplaceholder.typicode.com/users';
 
 export default class UserList extends React.Component {
-  state = {
-    users: [],
-  };
-
-  componentDidMount() {
-    fetch(API_URL)
-      .then((response) => response.json())
-      .then((data) =>
-        this.setState({
-          users: data,
-        })
-      )
-      .catch((error) => console.log(error));
-  }
-
   render() {
-    return (
-      <section>
-        <h1>Usuarios</h1>
-        <ul>
-          {this.state.users.map((user) => (
-            <li key={user.id}>{user.name}</li>
-          ))}
-        </ul>
-      </section>
-    );
+    return <h1>Usuarios</h1>;
   }
 }
